f45b5dc5150e7646af31e47ca3aa7bc35187dbaa
Zerobrofan <abdallahamirmonir@gmail.com>
2023-08-22 00:15:59 +0300

    first commit

---
+++ src/data/data.js
@@ -0,0 +1,29 @@
+import AWS from '../assets/projects/aws.png'
+import SoleStyle from '../assets/projects/sole.png'
+import Quotey from '../assets/projects/quotey.png'
+
+export const data=[
+    {
+        id:1,
+        name:"SoleStyle - eCommerce website",
+        image:SoleStyle,
+        github:"https://github.com/Zerobrofan/SoleStyle-eCommerce-website",
+        live:"https://zerobrofan.github.io/SoleStyle-eCommerce-website/index.html",
+    },
+    {
+        id:2,
+        name:"Quotey - Inspirational Quotes",
+        image:Quotey,
+        github:"https://github.com/Zerobrofan/Quotey-Inspirational-Quotes",
+        live:"https://quoteyextension.com/",
+    },
+    {
+        id:3,
+        name:"Static Website Hosting Using AWS S3",
+        image:AWS,
+        github:"https://github.com/Zerobrofan/Static-Website-Hosting-Using-AWS-S3",
+        live:"https://abdallahamir.website/",
+    },
+
+
+]
